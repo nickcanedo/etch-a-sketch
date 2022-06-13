f5a9601a13d8d41f2b830c133342ed72499dd457
Nicolas Rebollo <rebollo.nicolas@yahoo.com>
2022-06-13 18:33:39 -0400

    Add most game functionality except rainbow mode
    - Use several event listeners and functions to check for different scenarios and button clicks

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+// set default grid size to 16x16
+const grid = document.querySelector(".grid");
+
+let gridDimension = 16;
+window.addEventListener("load", gridSize(gridDimension));
+
+// add grid size changing functionality
+const sizeBtn = document.querySelector(".size");
+
+// create grid of any size
+let drag = false;
+
+function gridSize(size) {
+    if (size <= 100) {
+        for (let i = 0; i < size; i++) {
+            for (let j = 0; j < size; j++) {
+                const div = document.createElement("div");
+                div.style.backgroundColor = "var(--headline)";
+                div.style.width = `${450/size}px`;
+                div.style.height = `${450/size}px`;
+                div.classList.add("gridSquare")
+                div.addEventListener("mousedown", dragOn);
+                div.addEventListener("mousemove" , squareClick)
+                div.addEventListener("mouseup", dragOff);
+                grid.appendChild(div);
+            }
+        }
+    } else {
+        for (let i = 0; i < 100; i++) {
+            for (let j = 0; j < 100; j++) {
+                const div = document.createElement("div");
+                div.style.backgroundColor = "var(--headline)";
+                div.style.width = `${450/100}px`;
+                div.style.height = `${450/100}px`;
+                div.classList.add("gridSquare")
+                div.addEventListener("mousedown", dragOn);
+                div.addEventListener("mousemove" , squareClick)
+                div.addEventListener("mouseup", dragOff);
+                grid.appendChild(div);
+            }
+        }
+    }
+    
+}
+
+// clear grid of child divs
+function clearGrid() {
+    grid.innerHTML = '';
+}
+
 // adding button hover animations
 function mouseOver(e) {
     e.target.classList.add("hover");
@@ -25,3 +75,81 @@ const github = document.querySelector(".fa-brands");
 
 github.addEventListener("mouseover", githubAddClass);
 github.addEventListener("mouseleave", githubRemoveClass);
+
+// create event listener for color picker (save chosen color in currentColor variable)
+let currentColor;
+
+const colorPalette = document.querySelector(".color");
+
+function defaultColor() {
+    currentColor = "black";
+}
+window.addEventListener("load", defaultColor);
+
+colorPalette.addEventListener("input", function(e) {
+    currentColor = e.target.value;
+});
+
+// manage click event (hover can create unwanted drawing) for grid drawing
+function squareClick(e) {
+    if (drag) {
+        e.target.style.backgroundColor = currentColor;
+    }
+}
+
+function dragOn() {
+    drag = true;
+}
+
+function dragOff() {
+    drag = false;
+}
+
+// clear canvas
+const clearBtn = document.querySelector(".clear");
+
+clearBtn.addEventListener("click", function(e) {
+    clearGrid();
+    gridSize(gridDimension);
+})
+
+// add slider text to display grid size
+const sliderText = document.querySelector(".size");
+sliderText.textContent = `${gridDimension} x ${gridDimension}`;
+
+function updateSliderText() {
+    sliderText.textContent = `${gridDimension} x ${gridDimension}`;
+}
+
+// size-changing slider
+const slider = document.querySelector(".slider");
+
+function sliderChange(e) {
+    gridDimension = e.target.value;
+    updateSliderText();
+    clearGrid();
+    gridSize(gridDimension);
+}
+
+slider.addEventListener("input", sliderChange);
+
+// add eraser functionality
+const eraseBtn = document.querySelector(".eraser");
+
+let storageColor;
+
+function eraser(e) {
+    storageColor = currentColor;
+    currentColor = "#fffffe";
+}
+
+eraseBtn.addEventListener("click", eraser)
+
+// add draw mode functionality
+const drawBtn = document.querySelector(".draw");
+
+function drawMode(e) {
+    currentColor = storageColor;
+}
+
+drawBtn.addEventListener("click", drawMode);
